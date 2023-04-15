a9adf396c798ea5942a5da7a65dab74463882af8
Etai89 <123596508+Etai89@users.noreply.github.com>
2023-04-15 04:40:49 -0700

    updated files

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import sqlite3
+import winsound
+
+
+def clear_db():
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM users')
+    conn.commit()
+    conn.close()
+
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -8,22 +18,57 @@ class RequestHandler(BaseHTTPRequestHandler):
         post_body = self.rfile.read(content_len)
         data = json.loads(post_body)
 
-        print("Name:", data['name'])
+        print("Command:", data['name'])
 
+        # check if "clear data" is in the received data
+        if "clear data" in data['name']:
+            clear_db()  # call the clear_db function to clear the database file
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.wfile.write(b'Data cleared successfully')
+            return
 
-        # connect to the database and insert the data
+        # check if "print data" is in the received data
+        if "print data" in data['name']:
+            conn = sqlite3.connect('example.db')
+            c = conn.cursor()
+            c.execute('SELECT * FROM users')
+            rows = c.fetchall()
+            for row in rows:
+                print(row[0])
+            conn.close()
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            self.wfile.write(b'Data printed successfully')
+            return
+
+        # check if "money" is in the received data
+        if "money" in data['name']:
+            print('activate')
+            winsound.Beep(1350, 160)
+            response_to_website = "Ok brother"
+        else:
+            response_to_website = "Success"
+
+        # connect to the database and insert the name
         conn = sqlite3.connect('example.db')
         c = conn.cursor()
-        c.execute('CREATE TABLE IF NOT EXISTS users (name TEXT, email TEXT)')
-        c.execute('INSERT INTO users VALUES (?, ?)', (data['name']))
+        c.execute('CREATE TABLE IF NOT EXISTS users (name TEXT)')
+        c.execute('INSERT INTO users (name) VALUES (?)', (data['name'],))
         conn.commit()
         conn.close()
 
         # send a response to the client
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        self.wfile.write(b'Success')
+        self.wfile.write(response_to_website.encode())
 
     def do_OPTIONS(self):
         self.send_response(200)
@@ -33,11 +78,8 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
 
-server = HTTPServer(('localhost', 5000), RequestHandler)
-server.serve_forever()
-
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    server_address = ('', 5000)
     httpd = HTTPServer(server_address, RequestHandler)
     print('Starting server...')
     httpd.serve_forever()
