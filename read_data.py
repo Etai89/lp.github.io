ea2e4ea73f82af052b82fd271d83e37e56967254
Etai89 <123596508+Etai89@users.noreply.github.com>
2023-04-14 17:49:55 -0700

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,16 +1,29 @@
 import sqlite3
+import time
 
-# connect to the database and read data from the 'users' table
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
-c.execute('SELECT * FROM users')
-rows = c.fetchall()
+def clear_db():
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM users')
+    conn.commit()
+    conn.close()
 
-# print the data to the console
-for row in rows:
-    print('Name:', row[0])
-    print('Email:', row[1])
-    print('---------------------------------------')
+while True:
+    # connect to the database and read data from the 'users' table
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM users')
+    rows = c.fetchall()
 
-# close the database connection
-conn.close()
+    # print the data to the console
+    for row in rows:
+        print('Name:', row[0])
+        print('Email:', row[1])
+        print('---------------------------------------')
+
+    # close the database connection
+    conn.close()
+
+    # clear the database every 30 seconds
+    time.sleep(30)
+    #clear_db()
